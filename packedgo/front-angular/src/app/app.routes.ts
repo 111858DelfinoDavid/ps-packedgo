2ffc8e7b5bdeacc1a204439493a1bb874932389f
113973LUPARIA <113973@tecnicatura.frc.utn.edu.ar>
2025-10-29 00:41:11 -0300

    fix(frontend): Correct landing component import in routes

--- packedgo/front-angular/src/app/app.routes.ts
+++ packedgo/front-angular/src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
   // Landing page
   {
     path: '',
-    loadComponent: () => import('./features/landing/landing.component').then(m => m.LandingNewComponent)
+    loadComponent: () => import('./features/landing/landing.component').then(m => m.LandingComponent)
   },
   {
     path: 'home',
